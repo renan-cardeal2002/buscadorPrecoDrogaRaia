c34e6cad7bdae558bfa94149f23092593cbb42bb
renan_cardeal <renan.ribeiro@flsoft.com.br>
2022-12-08 08:35:41 -0300

    ok

--- pageController.js
+++ pageController.js
@@ -5,39 +5,10 @@ async function scrapeAll(browserInstance){
 	let browser;
 	try{
 		browser = await browserInstance;
-        let scrapedData = {}
-		//await pageScraper.scraper(browser);	
-		scrapedData = await pageScraper.scraper(browser);	
+
+		await pageScraper.scraper(browser);	
 
 		await browser.close();
-
-        // fs.writeFile("data.json", JSON.stringify(scrapedData), 'utf8', function(err) {
-		//     if(err) {
-		//         return console.log(err);
-		//     }
-		//     console.log("The data has been scraped and saved successfully! View it at './data.json'");
-		// });
-
-
-        // var axios = require('axios');
-
-        // var config = {
-        //     method: 'post',
-        //     url: 'http://192.168.25.60:3333/util/inserirBuscaPreco',
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjoiMDIxOTAxOTM5ODIiLCJzZW5oYSI6IlJPTkFMRE8iLCJpYXQiOjE2NzAzMjYyMDcsImV4cCI6MTY3MDM2MjIwN30.Upd-OwjrcDJNaH7Q4PjlOmiVBiOvIkkXGH-9z5TnZho",
-        //     },
-        //     data : JSON.stringify(scrapedData)
-        // };
-
-        // axios(config)
-        // .then(function (response) {
-        //     console.log(JSON.stringify(response.data));
-        // })
-        // .catch(function (error) {
-        //     console.log(error.data);
-        // });
     }
     catch(err){
         console.log("Could not resolve the browser instance => ", err);
--- pageScraper.js
+++ pageScraper.js
@@ -1,88 +1,86 @@
 const { Page } = require('puppeteer');
 
 const scraperObject = {
-	url: 'https://www.drogaraia.com.br/medicamentos.html',
+	url: 'https://www.drogaraia.com.br/medicamentos.html?page=7',
 	async scraper(browser){
 		let page = await browser.newPage();
-            console.log(`Navigating to ${this.url}...`);
-            await page.goto(this.url);
-			let scrapedData = [];
-            const allResultsSelector = '.dqiTTm';
-            const resultsSelector = '.products';
+        console.log(`Navegando para ${this.url}...`);
+        await page.goto(this.url, {timeout: 0});
+			
+        const allResultsSelector = '.rd-container-fluid .dqiTTm';
+        const resultsSelector = '.products';
 
-			async function scrapeCurrentPage(){
-				await page.waitForSelector(allResultsSelector);
-				let urls = await page.$$eval(resultsSelector, links => {
-					links = links.map(el => el.querySelector('a').href)
-					return links;
-				});
+		async function scrapeCurrentPage(){
+			await page.waitForSelector(allResultsSelector);
+			let urls = await page.$$eval(resultsSelector, links => {
+				links = links.map(el => el.querySelector('a').href)
+				return links;
+			});
 				
-				let pagePromise = (link) => new Promise(async(resolve, reject) => {
-					let dataObj = {};
-					let newPage = await browser.newPage();
-					await newPage.goto(link);
+			let pagePromise = (link) => new Promise(async(resolve, reject) => {
+				let dataObj = {};
+				let newPage = await browser.newPage();
+				await newPage.goto(link, {timeout: 0});
 
-					try{
-						dataObj['precoProduto'] = await newPage.$eval('.rd-row .rd-col-16 .eRXZWW .hVgczD .cPpbWR .ikFziU', text => text.textContent);
-					}
-					catch(err){
-					}
-					try{
-						dataObj['nomeProduto'] = await newPage.$eval('.rd-row .rd-col-16 .eRXZWW .gfVohE .title-product', text => text.textContent);
-					}
-					catch(err){
-					}
-					try{
-						dataObj['codFarma'] = await newPage.$eval('.rd-container-fluid .fRTSsN .eosUuQ .cUnSAh table tbody tr .gjcTgl', text => text.textContent);
-						dataObj['ean'] = await newPage.$eval('.rd-container-fluid .fRTSsN .eosUuQ .cUnSAh table tbody', text => text.textContent.split('EAN').pop());
-						dataObj.ean = await dataObj.ean.split('P').shift();
-					}
-					catch(err){
-					}
-
-					dataObj['farmacia'] = 'DROGA RAIA'
-					resolve(dataObj);
-					await newPage.close();
-				});
-
-				for(link in urls){
-					let currentPageData = await pagePromise(urls[link]);
-					var axios = require('axios');
-
-					var config = {
-						method: 'post',
-						url: 'http://192.168.25.60:3333/util/inserirBuscaPreco',
-						headers: {
-							"Content-Type": "application/json",
-							"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjoiMDIxOTAxOTM5ODIiLCJzZW5oYSI6IlJPTkFMRE8iLCJpYXQiOjE2NzA0MjEyOTAsImV4cCI6MTY3MDQ1NzI5MH0.HF5TAplVURZsvjsMfq86BsyKTJSlAa0EPv3OGwjmQFs",
-						},
-						data : JSON.stringify(currentPageData)
-					};
-
-					await axios(config)
-					.then(function (response) {
-					})
-					.catch(function (error) {
-					});
-					
-				}
-
-				let nextButtonExist = false;
 				try{
-					const nextButton = await page.$eval('.next-link > a', a => a.textContent);
-					nextButtonExist = true;
+					dataObj['precoProduto'] = await newPage.$eval('.rd-row .rd-col-16 .eRXZWW .hVgczD .cPpbWR .ikFziU', text => text.textContent);
 				}
 				catch(err){
-					nextButtonExist = false;
 				}
-				if(nextButtonExist){
-					await page.click('.next-link > a');
+				try{
+					dataObj['nomeProduto'] = await newPage.$eval('.rd-row .rd-col-16 .eRXZWW .gfVohE .title-product', text => text.textContent);
+				}
+				catch(err){
+				}
+				try{
+					dataObj['codFarma'] = await newPage.$eval('.rd-container-fluid .fRTSsN .eosUuQ .cUnSAh table tbody tr .gjcTgl', text => text.textContent);
+					dataObj['ean'] = await newPage.$eval('.rd-container-fluid .fRTSsN .eosUuQ .cUnSAh table tbody', text => text.textContent.split('EAN').pop());
+					dataObj.ean = await dataObj.ean.split('P').shift();
+				}
+				catch(err){
+				}
 
-					return scrapeCurrentPage(); // Call this function recursively
-				}
-				await page.close();
-				return scrapedData;
+				dataObj['farmacia'] = 'DROGA RAIA'
+				resolve(dataObj);
+				await newPage.close();
+			});
+
+			for(link in urls){
+				let currentPageData = await pagePromise(urls[link]);
+				var axios = require('axios');
+
+				var config = {
+					method: 'post',
+					url: 'http://192.168.25.60:3333/util/inserirBuscaPreco',
+					headers: {
+						"Content-Type": "application/json",
+						"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjoiMDIxOTAxOTM5ODIiLCJzZW5oYSI6IlJPTkFMRE8iLCJpYXQiOjE2NzA0OTkxMTMsImV4cCI6MTcwMjAzNTExM30.JqR38Jrk7AQZqIoqk2RLJsmpN7uwe01GQihg5AiB-ws",
+					},
+					data : JSON.stringify(currentPageData)
+				};
+
+				await axios(config)
+				.then(function (response) {
+				})
+				.catch(function (error) {
+				});		
 			}
+
+			let nextButtonExist = false;
+			try{
+				const nextButton = await page.$eval('.next-link > a', a => a.textContent);
+				nextButtonExist = true;
+			}
+			catch(err){
+				nextButtonExist = false;
+			}
+			if(nextButtonExist){
+				await page.click('.next-link > a');
+
+				return scrapeCurrentPage();
+			}
+			await page.close();
+		}
 		let data = await scrapeCurrentPage();
 		return data;
 	}
